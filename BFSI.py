05693ee29756a03053f60a4b83b13b9abcc0ff7f
LuisMorales <63946989+LuisM0ral3s@users.noreply.github.com>
2023-09-25 18:14:48 -0600

    BFSI.py Semifuncional
    El problema con este código es que no puede imprimir solo una iteración (declarando un 1 hace 2) y tiende a hacer una iteración mas de lo deseado

--- BFSI.py
+++ BFSI.py
@@ -1,27 +1,38 @@
-grafo = {
   'A' : ['B','C'],
   'B' : ['D', 'E'],
-  'C' : ['F'],
+  'C' : ['F','J'],
   'D' : [],
-  'E' : ['F'],
-  'F' : []
+  'E' : ['H'],
+  'F' : [],
+  'H':[],
+  'J':[]
 }
 
 visitado = [] 
 queue = []     
+prev=[]
 
-def bfs(visitado, grafo, nodo):
+def bfs(visitado, grafo, nodo,iter):
   visitado.append(nodo)
   queue.append(nodo)
-
+  prev=queue.copy()
   while queue:
     s = queue.pop(0)
-    print (s, end = " ")
+
+    if s not in prev:
+      prev=queue.copy()
+      iter-=1
+      if iter==0:
+        break
 
     for neighbour in grafo[s]:
       if neighbour not in visitado:
         visitado.append(neighbour)
         queue.append(neighbour)
+    
+
+  print(visitado)
+    
 
 
-bfs(visitado, grafo, 'A')
+bfs(visitado, grafo, 'A',3)
